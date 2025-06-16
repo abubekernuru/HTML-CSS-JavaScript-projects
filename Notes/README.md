e43cbc6a9bf8d0a0ac63d675eedb3028fbc1a2f5
abubekernuru <abubekernuru27@gmail.com>
2025-06-16 22:38:18 +0300

    Merge unrelated histories

---
+++ Notes/README.md
@@ -0,0 +1,11 @@
+
+# Notes App
+
+This folder contains the source code for the Notes App.
+
+## Deployment
+
+The app has been deployed on Vercel.  
+You can access it here : [notes-appank.vercel.app](https://notes-appank.vercel.app)
+
+
---
+++ Notes/script.js
@@ -0,0 +1,49 @@
+const adder = document.getElementById("add");
+
+adder.addEventListener("click", ()=>{
+    notesApp();
+})
+
+function notesApp(){
+    const noteApp = document.createElement('main');
+    noteApp.classList.add('note');
+    noteApp.innerHTML = `
+    <section class="notes">
+        <div class="tools">
+            <button class="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+            <button class="delete"><i class="fa-solid fa-trash"></i></button>
+        </div>
+        <textarea class="textarea hidden" id="textarea"></textarea>
+        <div class="display" id="display"></div>
+    </section>
+    `
+    const editBtn = noteApp.querySelector(".edit");
+    const deleteBtn = noteApp.querySelector(".delete");
+    const textarea = noteApp.querySelector("textarea");
+    const displayer = noteApp.querySelector(".display");
+
+    const text = textarea.value;
+    displayer.innerHTML = marked(text);
+
+    textarea.addEventListener("input", (e)=>{
+        const { value } = e.target;
+        displayer.innerHTML = marked(value);
+    })
+    
+    editBtn.addEventListener("click", () => {
+        if (textarea.classList.contains("hidden")) {
+            textarea.value = displayer.textContent;
+        }
+        textarea.classList.toggle("hidden");
+        displayer.classList.toggle("hidden");
+    })
+
+    deleteBtn.addEventListener("click", ()=>{
+        noteApp.remove();
+    })
+        document.body.appendChild(noteApp);
+    
+
+
+}
+notesApp();
