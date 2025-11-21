2d9bc4a315d5aa267776b061d6ac12e07c0596de
abubekernuru <abubekernuru27@gmail.com>
2025-11-21 17:38:23 +0300

    anime pics generator js

--- anime-pics-generator/script.js
+++ anime-pics-generator/script.js
@@ -1,7 +1,7 @@
 const btnEl = document.getElementById("btn");
 const animeContainerEl = document.querySelector(".anime-container");
-const animeImgEl = document.querySelector("anime-image");
-const animeNameEl = document.querySelector("alime-name");
+const animeImgEl = document.querySelector(".anime-image");
+const animeNameEl = document.querySelector(".anime-name");
 
 
 btnEl.addEventListener("click", async function(){
@@ -11,15 +11,16 @@ btnEl.addEventListener("click", async function(){
         btnEl.innerText = "Loading...";
         animeNameEl.innerText = "Updating...";
         animeImgEl.src = "./spinner.svg"
-        const url = "https://nekos.life/api/v2/img/neko";
-        const response = await fetch(url);
+        const response = await fetch('https://nekos.life/api/v2/img/neko');
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         btnEl.disabled = false;
+        btnEl.innerText = "Get Anime";
         animeImgEl.src = data.url;
-        animeNameEl.innerText = data.artist
+        animeNameEl.innerText = "Hello there!"
     } catch (error) {
         console.log(error);
+        btnEl.disabled = false;
         btnEl.innerText = "Get Anime";
         animeNameEl.innerText = "An error happened please try again later!";
     }
