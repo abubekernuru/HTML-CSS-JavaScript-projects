0fbeecbd7716cd6b2fcef6a077f7470e41d517e8
abubekernuru <abubekernuru27@gmail.com>
2026-01-24 15:19:21 +0300

    currency converter functionality added with api

--- currency-converter/script.js
+++ currency-converter/script.js
@@ -5,11 +5,17 @@ const firstCurrencyEl = document.getElementById("currency-first");
 const secondCurrencyEl = document.getElementById("currency-second");
 const firstValueEl = document.getElementById("worth-first");
 const secondValueEl = document.getElementById("worth-second");
+const exchangerateEl = document.getElementById("exchange-rate");
 
 firstCurrencyEl.addEventListener("change", updateCurrency);
 secondCurrencyEl.addEventListener("change", updateCurrency);
 firstValueEl.addEventListener("input", updateCurrency);
 
-function updateCurrency(){
-    console.log("called")
+async function updateCurrency(){
+    const res = await fetch(`https://v6.exchangerate-api.com/v6/aae4c3bcff39fc4ab910fdfd/latest/${firstCurrencyEl.value}`);
+    const data = await res.json();
+    // console.log(data)
+    let rates = data.conversion_rates[secondCurrencyEl.value]
+    secondValueEl.value = (rates * firstValueEl.value)
+    exchangerateEl.innerText = `1 ${firstCurrencyEl.value} ~ ${data.conversion_rates[secondCurrencyEl.value]} ${secondCurrencyEl.value}`
 }
