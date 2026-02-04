a6253b355919c84f825ecd739991a70931a318a7
abubekernuru <abubekernuru27@gmail.com>
2026-02-04 12:26:56 +0300

    complete weather app ui

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,1 +1,6 @@
-const Api_key = '825ba67370fffd3d35c21c3c88ca1170';
+const API_KEY = '825ba67370fffd3d35c21c3c88ca1170';
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+
