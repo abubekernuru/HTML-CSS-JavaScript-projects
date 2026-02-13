41038d734368b1e66e0c51ed26e984c1d3e60e1a
abubekernuru <abubekernuru27@gmail.com>
2026-02-13 22:10:12 +0300

    add show more functionality

--- image-search-app/script.js
+++ image-search-app/script.js
@@ -6,15 +6,45 @@ const access_key="GsmzhG83gQVUyP3AUfjdgZ2-3mFZwMcnQVPq67nXC-E";
 const formEl = document.getElementById('search-form');
 const inputEl = document.getElementById('search-input');
 const imageContEl = document.getElementById('image-results');
+const showMoreBtn = document.getElementById('show-more-btn');
 
 formEl.addEventListener('submit', searchImage);
+
+
+let query = "";
+let page = 1;
 
 async function searchImage(e){
     e.preventDefault();
     try {
         imageContEl.innerHTML=""
-        const query = inputEl.value;
-        const res = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${access_key}`);
+        page=1;
+        query = inputEl.value;
+        const res = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${access_key}&page=${page}`);
+        const data = await res.json();
+        console.log(data);
+        (data.results).forEach((result)=>{
+            const imageHtml =`
+                <div class="img-container">
+                    <img src='${result.urls.small}' alt='${result.alt_description}'/>
+                    <a href=${result.links.html} target='_blank'>${result.alt_description || "View on Unsplash"}</a>
+                </div>
+            `
+            imageContEl.insertAdjacentHTML("beforeend", imageHtml)
+        })
+        showMoreBtn.style.display = 'block';
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+showMoreBtn.addEventListener('click', loadMore)
+
+async function loadMore(e) {
+    try {
+        query = inputEl.value;
+        page++
+        const res = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${access_key}&page=${page}`);
         const data = await res.json();
         console.log(data);
         (data.results).forEach((result)=>{
